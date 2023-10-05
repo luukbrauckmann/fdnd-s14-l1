8826fc7d42cda67f338cd861e9fd191096a0e680
Siem.p <64197688+Siem-P@users.noreply.github.com>
2023-10-05 13:59:07 +0200

    First setup Players fetch

---
+++ src/routes/players/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let data
+    const { players } = data
+</script>
+
+
+<section class="all-teams">
+    {#each players as player}
+        <div>
+            <img src="/images/raimon.png" alt="Raimon Logo">
+            <p>{player.firstname}</p>
+            <p>{player.lastname}</p>
+            
+        </div>
+    {/each}
+</section>
