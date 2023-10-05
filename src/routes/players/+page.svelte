aecf827ad9cc08ec18b24c03c9c4147dc3f0b5e2
Siem.p <64197688+Siem-P@users.noreply.github.com>
2023-10-05 14:30:44 +0200

    Added playerdata + join from Countries

--- src/routes/players/+page.svelte
+++ src/routes/players/+page.svelte
@@ -4,12 +4,16 @@
 </script>
 
 
-<section class="all-teams">
+<section class="all-players">
     {#each players as player}
         <div>
             <img src="/images/raimon.png" alt="Raimon Logo">
             <p>{player.firstname}</p>
             <p>{player.lastname}</p>
+            <p>{player.gender}</p>
+            <p>{player.country.name}</p>
+            <p>{player.country.region}</p>
+            <p>{player.date_of_birth}</p>
             
         </div>
     {/each}
